70a4f575437a4c3c06cf0df74b5415ef1bdd7dbc
James Mitchell <jdm3@st-and.ac.uk>
2017-09-19 14:48:50 +0100

    TEMP

---
+++ src/orb.h
@@ -0,0 +1,121 @@
+//
+// libsemigroups - C++ library for semigroups and monoids
+// Copyright (C) 2017 James D. Mitchell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// This file contains ...
+
+#ifndef LIBSEMIGROUPS_SRC_ORB_H_
+#define LIBSEMIGROUPS_SRC_ORB_H_
+
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "semigroups.h"
+#include "timer.h"
+
+namespace libsemigroups {
+
+  template <typename ElementType, typename PointType> class Orb {
+   public:
+    Orb(std::vector<ElementType*> gens, PointType seed)
+        : _gens(gens),
+          _orb({seed}),
+          _map({std::make_pair(seed, 0)}),
+          _gen({nullptr}),
+          _parent({-1}) {
+      assert(!gens.empty());
+      if (_tmp.degree() < _gens[0].degree()) {
+        _tmp.copy(_gens[0]);
+      }
+    }
+
+    ~Orb();
+
+    void enumerate() {
+      for (size_t i = 0; i < _orb.size(); i++) {
+        for (ElementType const* x : _gens) {
+          PointType pt = (*x)[_orb[i]];
+          if (_map.find(pt) == _map.end()) {
+            _map.insert(std::make_pair(pt, _orb.size()));
+            _orb.push_back(pt);
+            _gen.push_back(x);
+            _parent.push_back(i);
+          }
+        }
+      }
+    }
+
+    // TODO change the name to find
+    size_t position(PointType pt) {
+      auto it = _map.find(pt);
+      if (it != _map.end()) {
+        return (*it).second;
+      } else {
+        return -1;
+      }
+    }
+
+    void reserve(size_t n) {
+      _map.reserve(n);
+      _orb.reserve(n);
+      _gen.reserve(n);
+    }
+
+    inline PointType operator[](size_t pos) const {
+      assert(pos < _orb.size());
+      return _orb[pos];
+    }
+
+    inline PointType at(size_t pos) const {
+      return _orb.at(pos);
+    }
+
+    size_t size() {
+      return _orb.size();
+    }
+
+    Element const* mapper(size_t pos) {
+      if (pos >= _mappers.size()) {
+        size_t i = pos;
+        assert(i < _orb.size());
+        Element const* out = _gen[i].really_copy();
+        Element const* tmp = out.really_copy();
+        while (out != nullptr) {
+          i = _parent[i];
+          out->redefine(_gen[i], tmp);
+          tmp.copy(out);
+        }
+        _mappers[i] = out;
+      }
+      return _mappers[pos];
+    }
+
+   private:
+    std::vector<ElementType*> _gens;
+    std::unordered_map<PointType, size_t> _map;
+    std::vector<PointType>    _orb;
+    std::vector<ElementType*> _gen;
+    std::vector<size_t>       _parent;
+    std::vector<ElementType*> _mappers;
+    static ElementType*       _tmp = new Permutation(new std::vector());
+  };
+}  // namespace libsemigroups
+
+#endif  // LIBSEMIGROUPS_SRC_SIMS_H_
---
+++ tests/orb.test.cc
@@ -0,0 +1,67 @@
+//
+// libsemigroups - C++ library for semigroups and monoids
+// Copyright (C) 2017 James D. Mitchell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "../src/orb.h"
+#include "catch.hpp"
+
+#define ORB_REPORT false
+
+using namespace libsemigroups;
+
+TEST_CASE("Semigroup 01: small transformation semigroup",
+          "[quick][semigroup][finite][01]") {
+  std::vector<Element*> gens = {new Transformation<u_int16_t>({0, 1, 0}),
+                                new Transformation<u_int16_t>({0, 1, 2})};
+  Semigroup S = Semigroup(gens);
+  S.set_report(SEMIGROUPS_REPORT);
+  really_delete_cont(gens);
+
+  REQUIRE(S.size() == 2);
+  REQUIRE(S.degree() == 3);
+  REQUIRE(S.nridempotents() == 2);
+  REQUIRE(S.nrgens() == 2);
+  REQUIRE(S.nrrules() == 4);
+
+  Element* expected = new Transformation<u_int16_t>({0, 1, 0});
+  REQUIRE(*S[0] == *expected);
+  expected->really_delete();
+  delete expected;
+
+  expected = new Transformation<u_int16_t>({0, 1, 2});
+  REQUIRE(*S[1] == *expected);
+  expected->really_delete();
+  delete expected;
+
+  Element* x = new Transformation<u_int16_t>({0, 1, 0});
+  REQUIRE(S.position(x) == 0);
+  REQUIRE(S.test_membership(x));
+  x->really_delete();
+  delete x;
+
+  x = new Transformation<u_int16_t>({0, 1, 2});
+  REQUIRE(S.position(x) == 1);
+  REQUIRE(S.test_membership(x));
+  x->really_delete();
+  delete x;
+
+  x = new Transformation<u_int16_t>({0, 0, 0});
+  REQUIRE(S.position(x) == Semigroup::UNDEFINED);
+  REQUIRE(!S.test_membership(x));
+  x->really_delete();
+  delete x;
+}
