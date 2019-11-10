68ae6fff7a38e016a1271840fa2a9d72eb11c911
James D. Mitchell <jdm3@st-andrews.ac.uk>
2019-11-10 19:52:41 +0000

    Try again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ import sphinx_rtd_theme
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if on_rtd:
     subprocess.call(
-        "cd ../../ && etc/make-doc-yml.sh && etc/make-doc-sphinx.sh",
+        "cd ../../ && etc/make-doc-yml.sh",
         shell=True,
     )
 
